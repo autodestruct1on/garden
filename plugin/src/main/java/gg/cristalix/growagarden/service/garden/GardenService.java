89a5724aeae8d18a50e008295207eedef0e7d80b
autodestruct1on <isaburmistrova@gmail.com>
2025-10-16 10:00:40 +0300

    Add files via upload

--- plugin/src/main/java/gg/cristalix/growagarden/service/garden/GardenService.java
+++ plugin/src/main/java/gg/cristalix/growagarden/service/garden/GardenService.java
@@ -9,10 +9,7 @@ import gg.cristalix.growagarden.model.garden.vegetation.SeedInstance;
 import gg.cristalix.growagarden.model.item.CropCustomItem;
 import gg.cristalix.growagarden.model.player.GamePlayer;
 import gg.cristalix.growagarden.model.world.WorldState;
-import gg.cristalix.growagarden.service.seed.SeedService;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.experimental.FieldDefaults;
+import lombok.experimental.UtilityClass;
 import ru.cristalix.core.math.V3;
 
 import javax.annotation.Nullable;
@@ -20,62 +17,21 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@UtilityClass
 public class GardenService {
 
   private static final double MIN_PLANT_DISTANCE = 0.15;
 
-  @Getter
-  static GardenService instance = new GardenService();
-
-  GrowAGardenPlugin plugin;
-  SeedService seedService;
-
-  private GardenService() {
-    this.plugin = GrowAGardenPlugin.getInstance();
-    this.seedService = plugin.getSeedService();
-  }
-
-  public static boolean canPlantAt(GamePlayer player, V3 position) {
+  public boolean canPlantAt(GamePlayer player, V3 position) {
     return !player.getGarden().hasPlantInRadius(position, MIN_PLANT_DISTANCE);
   }
 
-  public static boolean plantSeed(GamePlayer player, V3 position, String seedId) {
-    return instance.doPlantSeed(player, position, seedId);
-  }
-
-  @Nullable
-  public static CropCustomItem harvestCrop(GamePlayer player, V3 position, WorldState worldState) {
-    return instance.doHarvestCrop(player, position, worldState);
-  }
-
-  public static boolean waterPlant(GamePlayer player, V3 position) {
-    return instance.doWaterPlant(player, position);
-  }
-
-  public static boolean digPlant(GamePlayer player, V3 position) {
-    return instance.doDigPlant(player, position);
-  }
-
-  @Nullable
-  public static CellData getCellAt(GamePlayer player, V3 position) {
-    return player.getGarden().getCellAtPosition(position);
-  }
-
-  public static boolean hasPlantAt(GamePlayer player, V3 position) {
-    return player.getGarden().hasPlantAtPosition(position);
-  }
-
-  public static double getGrowthProgress(CellData cell, WorldState worldState) {
-    return instance.doGetGrowthProgress(cell, worldState);
-  }
-
-  private boolean doPlantSeed(GamePlayer player, V3 position, String seedId) {
+  public boolean plantSeed(GamePlayer player, V3 position, String seedId) {
     if (!canPlantAt(player, position)) {
       return false;
     }
 
-    SeedData seedData = seedService.getSeedData(seedId);
+    SeedData seedData = GrowAGardenPlugin.getInstance().getSeedService().getSeedData(seedId);
     if (seedData == null) {
       return false;
     }
@@ -95,14 +51,14 @@ public class GardenService {
   }
 
   @Nullable
-  private CropCustomItem doHarvestCrop(GamePlayer player, V3 position, WorldState worldState) {
+  public CropCustomItem harvestCrop(GamePlayer player, V3 position, WorldState worldState) {
     CellData cell = player.getGarden().getCellAtPosition(position);
     if (cell == null || cell.getSeedInstance() == null) {
       return null;
     }
 
     SeedInstance instance = cell.getSeedInstance();
-    SeedData seedData = seedService.getSeedData(instance.getSeedId());
+    SeedData seedData = GrowAGardenPlugin.getInstance().getSeedService().getSeedData(instance.getSeedId());
 
     if (seedData == null || !instance.isFullyGrown(seedData, worldState)) {
       return null;
@@ -127,7 +83,7 @@ public class GardenService {
     return crop;
   }
 
-  private boolean doWaterPlant(GamePlayer player, V3 position) {
+  public boolean waterPlant(GamePlayer player, V3 position) {
     CellData cell = player.getGarden().getCellAtPosition(position);
     if (cell == null || cell.getSeedInstance() == null) {
       return false;
@@ -142,7 +98,7 @@ public class GardenService {
     return true;
   }
 
-  private boolean doDigPlant(GamePlayer player, V3 position) {
+  public boolean digPlant(GamePlayer player, V3 position) {
     CellData cell = player.getGarden().getCellAtPosition(position);
     if (cell == null || cell.getSeedInstance() == null) {
       return false;
@@ -152,13 +108,22 @@ public class GardenService {
     return true;
   }
 
-  private double doGetGrowthProgress(CellData cell, WorldState worldState) {
+  @Nullable
+  public CellData getCellAt(GamePlayer player, V3 position) {
+    return player.getGarden().getCellAtPosition(position);
+  }
+
+  public boolean hasPlantAt(GamePlayer player, V3 position) {
+    return player.getGarden().hasPlantAtPosition(position);
+  }
+
+  public double getGrowthProgress(CellData cell, WorldState worldState) {
     if (cell == null || cell.getSeedInstance() == null) {
       return 0.0;
     }
 
     SeedInstance instance = cell.getSeedInstance();
-    SeedData seedData = seedService.getSeedData(instance.getSeedId());
+    SeedData seedData = GrowAGardenPlugin.getInstance().getSeedService().getSeedData(instance.getSeedId());
 
     if (seedData == null) {
       return 0.0;
